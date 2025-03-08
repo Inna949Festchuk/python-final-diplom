046a23471d28cb4f244c3872bcce497b2dde7925
Inna949Festchuk <innabori@mail.ru>
2025-03-08 13:38:40 +0200

    v0.0.5 Update apps.py

--- reference/netology_pd_diplom/backend/apps.py
+++ reference/netology_pd_diplom/backend/apps.py
@@ -1,12 +1,13 @@
 from django.apps import AppConfig
 
-
 class BackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend'
 
     def ready(self):
-        """
-        импортируем сигналы
-        """
-        # import backend.signals  # Импорт сигналов при старте приложения
+        # Импортируем обработчики сигналов здесь, чтобы избежать циклических импортов
+        from backend.signals import (
+            password_reset_token_created, 
+            new_user_registered_signal, 
+            new_order_signal
+        )
